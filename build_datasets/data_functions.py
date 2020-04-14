f983de577bf4b065bba8265f76e7a463d5894f1f
Leevisir <weijianli2021@u.northwestern.edu>
2020-04-13 21:33:31 -0500

    Add function stephanie_generate_elo(df, k1, k2)

--- build_datasets/data_functions.py
+++ build_datasets/data_functions.py
@@ -99,6 +99,43 @@ def generate_elo(df,counts_i):
         df['w_sf_elo'], df['l_sf_elo'] = surface_elo_1s, surface_elo_2s
     return df
 
+# takes in a dataframe of matches in atp/wta format and returns the dataframe with elo columns
+def stephanie_generate_elo(df, k1, k2):
+    players_list = np.union1d(df.w_name, df.l_name)
+    players_elo = dict(zip(list(players_list), [elo.Rating() for __ in range(len(players_list))]))
+    surface_elo = {}
+    for surface in ('Hard','Clay','Grass'):
+        surface_elo[surface] = dict(zip(list(players_list), [elo.Rating() for __ in range(len(players_list))])) 
+
+    elo_1s, elo_2s = [],[]
+    surface_elo_1s, surface_elo_2s = [],[]
+    elo_obj = elo.Elo_Rater()
+
+    # update player elo from every recorded match
+    for i, row in df.iterrows():
+        surface = row['surface']; is_gs = row['is_gs']
+        # append elos, rate, update
+        w_elo,l_elo = players_elo[row['w_name']],players_elo[row['l_name']]
+        delta1, delta2 = row['w_delta1'], row['w_delta2']
+        elo_1s.append(w_elo.value);elo_2s.append(l_elo.value)
+        
+        elo_obj.stephanie_rate_1vs1(w_elo,l_elo, k1, k2, delta1, delta2)
+
+        
+        surface_elo_1s.append(surface_elo[surface][row['w_name']].value if surface in ('Hard','Clay','Grass') else w_elo.value)
+        surface_elo_2s.append(surface_elo[surface][row['l_name']].value if surface in ('Hard','Clay','Grass') else l_elo.value)
+        if surface in ('Hard','Clay','Grass'):
+            new_elo1, new_elo2 = elo_obj.stephanie_rate_1vs1(surface_elo[surface][row['w_name']],surface_elo[surface][row['l_name']], k1, k2, delta1, delta2)
+
+    # add columns
+    if counts_i:
+        df['w_elo_538'], df['l_elo_538'] = elo_1s, elo_2s
+        df['w_sf_elo_538'], df['l_sf_elo_538'] = surface_elo_1s, surface_elo_2s
+    else:
+        df['w_elo'], df['l_elo'] = elo_1s, elo_2s
+        df['w_sf_elo'], df['l_sf_elo'] = surface_elo_1s, surface_elo_2s
+    return df
+
 
 def generate_52_stats(df,start_ind):
     players_stats = {}
