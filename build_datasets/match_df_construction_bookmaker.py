0eb1e094cb0d3c102141acdafee4a3244b84fca7
JingyaXun <jxun@sandiego.edu>
2020-04-18 13:11:19 -0500

    update imports

--- build_datasets/match_df_construction_bookmaker.py
+++ build_datasets/match_df_construction_bookmaker.py
@@ -8,7 +8,6 @@ ONLY_PBP = 0
 
 import sys
 sys.path.insert(0,SCRIPT_PATH)
-from tennisMatchProbability import matchProb
 from helper_functions import *
 from data_functions import *
 import pandas as pd
